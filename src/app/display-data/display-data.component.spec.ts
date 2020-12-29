5c8bda0bf3d6652323f75d6d07650bf20b2d3526
shubhamMandavgade <shubhamdmandavgade@gmail.com>
2020-12-29 23:48:24 +0530

    Changes

--- src/app/display-data/display-data.component.spec.ts
+++ src/app/display-data/display-data.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DisplayDataComponent } from './display-data.component';
 
@@ -6,7 +6,7 @@ describe('DisplayDataComponent', () => {
   let component: DisplayDataComponent;
   let fixture: ComponentFixture<DisplayDataComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DisplayDataComponent ]
     })
